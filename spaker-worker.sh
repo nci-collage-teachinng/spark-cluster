7cce41779acd9ffb0a9d0f31ac5e47e877f7b5ef
Sumit <trsumit@users.noreply.github.com>
2020-12-08 19:28:37 +0000

    Create spaker-worker.sh

---
+++ spaker-worker.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get -y install default-jre
+sudo apt-get -y install scala
+wget https://mirrors.whoishostingthis.com/apache/spark/spark-3.0.1/spark-3.0.1-bin-hadoop3.2.tgz
+tar -xvf spark-3.0.1-bin-hadoop3.2.tgz
+sudo mv spark-3.0.1-bin-hadoop3.2 /opt/spark
+echo "export SPARK_HOME=/opt/spark" >> ~/.profile
+echo "export PATH=$PATH:/opt/spark/bin:/opt/spark/sbin" >> ~/.profile
+echo "export PYSPARK_PYTHON=/usr/bin/python3" >> ~/.profile
+sudo apt-get install -y openssh-server openssh-client
